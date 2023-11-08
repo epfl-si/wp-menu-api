56fcfb0b9b10319d8d66a530191e4732eb0e84c5
Rosa Maggi <rosa.maggi@epfl.ch>
2023-11-08 15:01:31 +0100

    [Fix] clear code from console.log

--- src/menus/lists.ts
+++ src/menus/lists.ts
@@ -6,10 +6,8 @@ export class Lists {
 
     static searchAllParentsEntriesByID = (entry: WpMenu, urlInstanceRestUrl: string, siteArray: SiteTreeInstance): WpMenu[] => {
         const parent: { [urlInstance : string]: WpMenu } | undefined = siteArray.getParent(urlInstanceRestUrl,entry.ID);
-        console.log('urlInstanceRestUrl', urlInstanceRestUrl);
         if (parent) {
             const newUrl = Object.keys(parent)[0];
-            console.log('newUrl', newUrl);
             if (parent[newUrl]) {
                 const parents: WpMenu[] = this.searchAllParentsEntriesByID(parent[newUrl], newUrl , siteArray);
                 return [...parents, parent[newUrl]];
@@ -30,13 +28,11 @@ export class Lists {
             if (firstSite) {
                 const restUrl = Object.keys(firstSite)[0];
                 if (firstSite[restUrl]) {
-                    console.log('first site : ',firstSite[restUrl]);
                     switch ( type ) {
                         case "siblings":
                             items = siteArray.getSiblings(restUrl,firstSite[restUrl].ID);
                             break;
                         case "breadcrumb":
-                            console.log('first site breadcrumb',firstSite[restUrl], '::::restUrl:', restUrl)
                             items = [...this.searchAllParentsEntriesByID(firstSite[restUrl], restUrl, siteArray)];
                             break;
                     }
