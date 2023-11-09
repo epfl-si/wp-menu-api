cf3808fe8b2ac0f07b8a0b9226be855ec1bf544f
Rosa Maggi <rosa.maggi@epfl.ch>
2023-11-09 15:51:01 +0100

    [Refactor] Transform  class to module

--- src/menus/lists.ts
+++ src/menus/lists.ts
@@ -1,44 +1,47 @@
 import {WpMenu} from "../interfaces/wpMenu";
 import {SiteTreeInstance} from "../interfaces/siteTree";
-import {Refresh} from "./refresh";
+import {getArraySiteTreeByLanguage} from "./refresh";
 
-export class Lists {
 
-    static searchAllParentsEntriesByID = (entry: WpMenu, urlInstanceRestUrl: string, siteArray: SiteTreeInstance): WpMenu[] => {
-        const parent: { [urlInstance : string]: WpMenu } | undefined = siteArray.getParent(urlInstanceRestUrl,entry.ID);
-        if (parent) {
-            const newUrl = Object.keys(parent)[0];
-            if (parent[newUrl]) {
-                const parents: WpMenu[] = this.searchAllParentsEntriesByID(parent[newUrl], newUrl , siteArray);
-                return [...parents, parent[newUrl]];
-            }else {
-                return [];
-            }
+function searchAllParentsEntriesByID(entry: WpMenu, urlInstanceRestUrl: string, siteArray: SiteTreeInstance): WpMenu[] {
+    const parent: { [urlInstance : string]: WpMenu } | undefined = siteArray.getParent(urlInstanceRestUrl,entry.ID);
+
+    if (parent) {
+        const newUrl = Object.keys(parent)[0];
+
+        if (parent[newUrl]) {
+            const parents: WpMenu[] = searchAllParentsEntriesByID(parent[newUrl], newUrl , siteArray);
+            return [...parents, parent[newUrl]];
         } else {
             return [];
         }
+    } else {
+        return [];
     }
+}
 
-    static getMenuItems(url: string, lang: string, type: string) : WpMenu[] {
-        let items: WpMenu[] = [];
+export function getMenuItems (url: string, lang: string, type: string) : WpMenu[] {
+    let items: WpMenu[] = [];
+    let siteArray: SiteTreeInstance | undefined = getArraySiteTreeByLanguage(lang);
 
-        let siteArray: SiteTreeInstance | undefined = Refresh.getArraySiteTreeByLanguage(lang);
-        if (siteArray) {
-            let firstSite: { [urlInstance: string]: WpMenu } | undefined = siteArray.findItemByUrl(url);
-            if (firstSite) {
-                const restUrl = Object.keys(firstSite)[0];
-                if (firstSite[restUrl]) {
-                    switch ( type ) {
-                        case "siblings":
-                            items = siteArray.getSiblings(restUrl,firstSite[restUrl].ID);
-                            break;
-                        case "breadcrumb":
-                            items = [...this.searchAllParentsEntriesByID(firstSite[restUrl], restUrl, siteArray)];
-                            break;
-                    }
+    if (siteArray) {
+        let firstSite: { [urlInstance: string]: WpMenu } | undefined = siteArray.findItemByUrl(url);
+
+        if (firstSite) {
+            const restUrl = Object.keys(firstSite)[0];
+
+            if (firstSite[restUrl]) {
+                switch ( type ) {
+                    case "siblings":
+                        items = siteArray.getSiblings(restUrl,firstSite[restUrl].ID);
+                        break;
+                    case "breadcrumb":
+                        items = [...searchAllParentsEntriesByID(firstSite[restUrl], restUrl, siteArray)];
+                        break;
                 }
             }
         }
-        return items;
     }
+
+    return items;
 }
--- src/menus/refresh.ts
+++ src/menus/refresh.ts
@@ -4,6 +4,7 @@ import {ErrorResult, MenuAPIResult} from "../interfaces/menuAPIResult";
 import {SiteTree, SiteTreeInstance} from "../interfaces/siteTree";
 import {WpMenu} from "../interfaces/wpMenu";
 
+
 const headers: Headers = new Headers();
 headers.set('Content-Type', 'application/json');
 headers.set('Accept', 'application/json');
@@ -18,108 +19,126 @@ const arrayMenusFR: { urlInstanceRestUrl: string, entries: WpMenu[] }[] = [];
 const arrayMenusEN: { urlInstanceRestUrl: string, entries: WpMenu[] }[] = [];
 const arrayMenusDE: { urlInstanceRestUrl: string, entries: WpMenu[] }[] = [];
 
-export class Refresh {
-    static getSiteListFromWPVeritas(): Promise<Site[]> {
-        const request: RequestInfo = new Request(wpVeritasURL, {
-            method: 'GET',
-            headers: headers
-        });
+function getSiteListFromWPVeritas(): Promise<Site[]> {
+    const request: RequestInfo = new Request(wpVeritasURL, {
+        method: 'GET',
+        headers: headers
+    });
 
-        return fetch(request).then(res => res.json()).then(res => {
-            return res as Site[];
-        });
+    return fetch(request).then(res => res.json()).then(res => {
+        return res as Site[];
+    });
+}
+
+async function getMenusInParallel(
+    sites: Site[],
+    lang: string,
+    fn: (siteURL: string, language: string) => Promise<MenuAPIResult>, 
+    threads = 10
+): Promise<MenuAPIResult[]> {
+    const result: MenuAPIResult[][] = [];
+    const arr: Site[] = [];
+
+    sites.forEach(s => arr.push(s));
+
+    while (arr.length) {
+        let subListOfSitesMenus: Promise<MenuAPIResult>[] = arr.splice(0, threads).map(x => fn(x.url, lang));
+        const res: MenuAPIResult[] = await Promise.all(subListOfSitesMenus);
+        result.push(res);
     }
 
-    static async getMenusInParallel(sites: Site[], lang: string, fn: (siteURL: string, language: string) => Promise<MenuAPIResult>, threads = 10): Promise<MenuAPIResult[]> {
-        const result: MenuAPIResult[][] = [];
-        const arr: Site[] = [];
-        sites.forEach(s => arr.push(s));
-        while (arr.length) {
-            let subListOfSitesMenus: Promise<MenuAPIResult>[] = arr.splice(0, threads).map(x => fn(x.url, lang));
-            const res: MenuAPIResult[] = await Promise.all(subListOfSitesMenus);
-            result.push(res);
-        }
-        return result.flat();
+    return result.flat();
+}
+
+async function getMenuForSite(siteURL: string, lang: string): Promise<MenuAPIResult> {
+    if (protocolHostAndPort.indexOf('wp-httpd')>-1) {
+        siteURL = siteURL.replace("http://wp-httpd.epfl.ch",protocolHostAndPort);
     }
 
-    static getMenuForSite(siteURL: string, lang: string): Promise<MenuAPIResult> {
-        if (protocolHostAndPort.indexOf('wp-httpd')>-1) {
-            siteURL = siteURL.replace("http://wp-httpd.epfl.ch",protocolHostAndPort);
-        }
-        const siteMenuURL: string = siteURL.concat(restUrlEnd).concat(lang);
-        const request: RequestInfo = new Request(siteMenuURL, {
-            method: 'GET',
-            headers: headers
-        });
+    const siteMenuURL: string = siteURL.concat(restUrlEnd).concat(lang);
+    const request: RequestInfo = new Request(siteMenuURL, {
+        method: 'GET',
+        headers: headers
+    });
+    const timeoutPromise = new Promise<MenuAPIResult>(resolve => {
+        setTimeout(resolve.bind(null, new ErrorResult(siteMenuURL.concat(" - Timeout 10s"))), 10000);
+    });
 
-        const timeoutPromise = new Promise<MenuAPIResult>(resolve => {
-            setTimeout(resolve.bind(null, new ErrorResult(siteMenuURL.concat(" - Timeout 10s"))), 10000);
-        });
+    return Promise.race([
+        fetch(request).then((res) => res.json()).then((res) => res as MenuAPIResult),
+        timeoutPromise
+    ]).then((result) => {
+        if (result.status && result.status === 'OK') {
+            const siteUrlSubstring = siteMenuURL.substring(siteMenuURL.indexOf(protocolHostAndPort)+protocolHostAndPort.length);
 
-        return Promise.race([
-            fetch(request).then((res) => res.json()).then((res) => res as MenuAPIResult),
-            timeoutPromise
-        ]).then((result) => {
-            if (result.status && result.status==='OK'){
-                const siteUrlSubstring = siteMenuURL.substring(siteMenuURL.indexOf(protocolHostAndPort)+protocolHostAndPort.length);
-                switch ( lang ) {
-                    case "fr":
-                        arrayMenusFR.push( { urlInstanceRestUrl: siteUrlSubstring, entries: result.items } );
-                        break;
-                    case "de":
-                        arrayMenusDE.push( { urlInstanceRestUrl: siteUrlSubstring, entries: result.items } );
-                        break;
-                    default: //en
-                        arrayMenusEN.push( { urlInstanceRestUrl: siteUrlSubstring, entries: result.items } );
-                        break;
-                }
-                return result;
-            } else {
-                errors = errors.concat("\n").concat(siteMenuURL).concat(" - ").concat(JSON.stringify(result));
-                return new ErrorResult(siteMenuURL.concat(" - ").concat(result.status));
+            switch ( lang ) {
+                case "fr":
+                    arrayMenusFR.push( { urlInstanceRestUrl: siteUrlSubstring, entries: result.items } );
+                    break;
+                case "de":
+                    arrayMenusDE.push( { urlInstanceRestUrl: siteUrlSubstring, entries: result.items } );
+                    break;
+                default: //en
+                    arrayMenusEN.push( { urlInstanceRestUrl: siteUrlSubstring, entries: result.items } );
+                    break;
             }
-        }).catch ((error) => {
-            let message: string = '';
-            if (typeof error === "string") {
-                message = error;
-            } else if (error instanceof Error) {
-                message = error.message;
-            }
-            errors = errors.concat("\n").concat(siteMenuURL).concat(" - ").concat(message);
-            return new ErrorResult(siteMenuURL.concat(" - ").concat(message));
-        });
-    }
 
-    static async refreshMenu(): Promise<string> {
-        errors = '';
-        const sites = await this.getSiteListFromWPVeritas();
-        const filteredListOfSites: Site[] = sites.filter(function (site){
-            return openshiftEnv.includes(site.openshiftEnv);
-        });
-        const promises: Promise<MenuAPIResult[]>[] = [
-            this.getMenusInParallel(filteredListOfSites, "en", this.getMenuForSite, 10),
-            this.getMenusInParallel(filteredListOfSites, "fr", this.getMenuForSite, 10),
-            this.getMenusInParallel(filteredListOfSites, "de", this.getMenuForSite, 10)
-        ];
-        await Promise.all(promises);
-        if(errors!=='')
-            console.log(errors);//TODO where we should write errors?
-        return errors;
-    }
+            return result;
+        } else {
+            errors = errors.concat("\n").concat(siteMenuURL).concat(" - ").concat(JSON.stringify(result));
 
-    static getArraySiteTreeByLanguage(lang: string): SiteTreeInstance | undefined {
-        let siteArray: SiteTreeInstance;
-        switch ( lang ) {
-            case "fr":
-                siteArray = SiteTree(arrayMenusFR);
-                break;
-            case "de":
-                siteArray = SiteTree(arrayMenusDE);
-                break;
-            default: //en
-                siteArray = SiteTree(arrayMenusEN);
-                break;
+            return new ErrorResult(siteMenuURL.concat(" - ").concat(result.status));
         }
-        return siteArray;
+    }).catch ((error) => {
+        let message: string = '';
+
+        if (typeof error === "string") {
+            message = error;
+        } else if (error instanceof Error) {
+            message = error.message;
+        }
+
+        errors = errors.concat("\n").concat(siteMenuURL).concat(" - ").concat(message);
+
+        return new ErrorResult(siteMenuURL.concat(" - ").concat(message));
+    });
+}
+
+export async function refreshMenu(): Promise<string> {
+    errors = '';
+    const sites = await getSiteListFromWPVeritas();
+    const filteredListOfSites: Site[] = sites.filter(function (site){
+        return openshiftEnv.includes(site.openshiftEnv);
+    });
+    const promises: Promise<MenuAPIResult[]>[] = [
+        getMenusInParallel(filteredListOfSites, "en", getMenuForSite, 10),
+        getMenusInParallel(filteredListOfSites, "fr", getMenuForSite, 10),
+        getMenusInParallel(filteredListOfSites, "de", getMenuForSite, 10)
+    ];
+
+    await Promise.all(promises);
+
+    if (errors !== '') {
+        console.log(errors);  //TODO where we should write errors?
     }
+
+    return errors;
+}
+
+export function getArraySiteTreeByLanguage(lang: string): SiteTreeInstance | undefined {
+    let siteArray: SiteTreeInstance;
+
+    switch ( lang ) {
+        case "fr":
+            siteArray = SiteTree(arrayMenusFR);
+            break;
+        case "de":
+            siteArray = SiteTree(arrayMenusDE);
+            break;
+        default: //en
+            siteArray = SiteTree(arrayMenusEN);
+            break;
+    }
+
+    return siteArray;
 }
