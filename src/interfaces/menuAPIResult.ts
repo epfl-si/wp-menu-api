b0fb7f0cde2689609b23c1a6a5047bdb5ea897f5
Rosa Maggi <rosa.maggi@epfl.ch>
2024-09-20 10:49:28 +0200

    [Refactor] delete useless files

--- src/interfaces/menuAPIResult.ts
+++
@@ -1,9 +0,0 @@
-import { WpMenu } from './wpMenu'
-import {Subscribe} from './subscribe'
-import {HrefForSubscribe} from "./hrefForSubscribe";
-
-export interface MenuAPIResult {
-    status: string,
-    items: WpMenu[],
-    _links: Subscribe
-}
