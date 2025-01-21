9cf23dc3e6e485b6c7ec577aad890de868dc8e17
Rosa Maggi <rosa.maggi@epfl.ch>
2025-01-21 17:20:56 +0100

    [test] add some tests for kubernetes client

---
+++ test/unit/kubernetesClient.ts
@@ -0,0 +1,22 @@
+import {KubeConfig} from "@kubernetes/client-node";
+import {getOpenshift4PodName, getSiteListFromKubernetes} from "../../src/utils/source";
+import {assert, expect} from "chai";
+
+describe('Kubernetes Client Test', () => {
+    it('should initialize KubeConfig', () => {
+        const kc = new KubeConfig();
+        kc.loadFromDefault();
+    });
+    it('should find wp-nginx pod', async () => {
+        const kc = new KubeConfig();
+        kc.loadFromDefault();
+        const podName = await getOpenshift4PodName("svc0041t-wordpress")
+        assert(podName.indexOf("wp-nginx") > -1);
+    });
+    it('should find wordpresssite campus', async () => {
+        const kc = new KubeConfig();
+        kc.loadFromDefault();
+        const sites = await getSiteListFromKubernetes("svc0041t-wordpress");
+        expect(sites.find(site => site.url == 'https://wpn-test.epfl.ch/campus/restaurants-shops-hotels')).not.be.undefined;
+    });
+});
