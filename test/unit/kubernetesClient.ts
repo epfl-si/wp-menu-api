23a6e3266a2ea7e288e3e99a9f0da2409b277eeb
Rosa Maggi <rosa.maggi@epfl.ch>
2025-01-22 14:33:14 +0100

    [refactor] rename openshift4PodName -> K8SPodName

--- test/unit/kubernetesClient.ts
+++ test/unit/kubernetesClient.ts
@@ -1,5 +1,5 @@
 import {KubeConfig} from "@kubernetes/client-node";
-import {getOpenshift4PodName, getSiteListFromKubernetes} from "../../src/utils/source";
+import {getK8SPodName, getSiteListFromKubernetes} from "../../src/utils/source";
 import {assert, expect} from "chai";
 
 describe('Kubernetes Client Test', () => {
@@ -10,7 +10,7 @@ describe('Kubernetes Client Test', () => {
     it('should find wp-nginx pod', async () => {
         const kc = new KubeConfig();
         kc.loadFromDefault();
-        const podName = await getOpenshift4PodName("svc0041t-wordpress")
+        const podName = await getK8SPodName("svc0041t-wordpress")
         assert(podName.indexOf("wp-nginx") > -1);
     });
     it('should find wordpresssite campus', async () => {
